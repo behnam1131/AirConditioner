0003c841bcbabf297d5e9216f9d5a850b6272c9c
behnam1131 <akbari.behnam69@yahoo.com>
2019-05-27 18:10:10 +0430

    change 10

--- AirConditioner.Core/Dtos/UserExpertDto.cs
+++ AirConditioner.Core/Dtos/UserExpertDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace AirConditioner.Core.Dtos
@@ -7,7 +8,11 @@ namespace AirConditioner.Core.Dtos
     public class UserExpertDto
     {
         public int Id { get; set; }
+
+        [DisplayName("نام و نام خانوادگی")]
         public string Name { get; set; }
+
+        [DisplayName("موبایل")]
         public string Phone { get; set; }
     }
 }
---
+++ AirConditioner.Web/Controllers/UserExpertController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirConditioner.Core.Dtos;
+using AirConditioner.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirConditioner.Web.Controllers
+{
+    public class UserExpertController : Controller
+    {
+        private readonly IUserExpertService _userExpertService;
+        public UserExpertController(IUserExpertService userExpertService)
+        {
+            _userExpertService = userExpertService;
+
+        }
+
+        public IActionResult Index()
+        {
+            var list = _userExpertService.GetAll();
+            return View(list);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(UserExpertDto userExpertDto)
+        {
+
+            var flag = _userExpertService.Add(userExpertDto);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
